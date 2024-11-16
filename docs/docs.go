4c94693c5c3323c981213eada3f74c44965484e4
ALAO RAMON ADEBISI <alao.adebusy@gmail.com>
2024-11-16 18:42:13 +0000

    update code new

--- docs/docs.go
+++ docs/docs.go
@@ -1055,7 +1055,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "jellyfish-app-emxxl.ondigitalocean.app",
+	Host:             "https://jellyfish-app-emxxl.ondigitalocean.app",
 	BasePath:         "/",
 	Schemes:          []string{"http"},
 	Title:            "Cart Backend service",
