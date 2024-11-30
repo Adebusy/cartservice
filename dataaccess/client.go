ee65544aa3a1aa755c233b2190cb32d10ac1ce5a
ALAO RAMON ADEBISI <alao.adebusy@gmail.com>
2024-11-30 14:03:50 +0000

    update code new

--- dataaccess/client.go
+++ dataaccess/client.go
@@ -38,7 +38,7 @@ type ClientResp struct {
 func (cn DbConnect) GetClientByName(clientName string) TblClient {
 	res := TblClient{}
 	cn.DbGorm.Table("TblClient").Select("Id", "Name", "Status", "Description", "DateAdded").Where("\"Name\"=? and \"Status\"=1", clientName).First(&res)
-	fmt.Sprintln(res.Name)
+	fmt.Printf("name is %s", res.Name)
 	return res
 }
 
