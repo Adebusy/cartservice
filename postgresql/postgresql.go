9138f0e675716210c0b1e34652ab1f4c0538784d
ALAO RAMON ADEBISI <alao.adebusy@gmail.com>
2025-03-22 17:43:58 +0000

    update for env

--- postgresql/postgresql.go
+++ postgresql/postgresql.go
@@ -7,7 +7,6 @@ import (
 
 	dbSchema "github.com/Adebusy/cartbackendsvc/dataaccess"
 	"github.com/Adebusy/cartbackendsvc/obj"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -18,11 +17,13 @@ var DbGorm *gorm.DB
 var err error
 
 func GetDB() *gorm.DB {
-	if loadEnv := godotenv.Load(); loadEnv != nil {
-		ret := fmt.Sprintf("Unable to load environment variable. %s", loadEnv.Error())
-		fmt.Println(ret)
-	}
-	env := "local"
+	// if loadEnv := godotenv.Load(); loadEnv != nil {
+	// 	ret := fmt.Sprintf("Unable to load environment variable. %s", loadEnv.Error())
+	// 	fmt.Println(ret)
+	// }
+
+	env := os.Getenv("env")
+
 	SERVER := os.Getenv("DATABASE_SERVER")
 	USERID := os.Getenv("USERID")
 	DATABASE := os.Getenv("DATABASE")
